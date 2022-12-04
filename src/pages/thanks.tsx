a56320bb1e5be6e1bb46a1078a90112da3091973
ibadus <ibadus@icloud.com>
2022-12-04 21:01:52 +0100

    edited thanks message

--- src/pages/thanks.tsx
+++ src/pages/thanks.tsx
@@ -3,11 +3,9 @@ export default function Thanks() {
 		<div className="space-y-4 py-10">
 			<h1 className="text-5xl font-bold">Thank you!</h1>
 			<p className="opacity-80">
-				I'll be in touch soon. Thanks for reaching out! In the meantime, feel
-				free to checkout my GitHub linked on the home page, there's some cool
-				things on there 😉
+				I'll be in touch soon. Thanks for reaching out!
 				<br />
-				<br />– Ali
+				<br />– ibadus
 			</p>
 		</div>
 	);
