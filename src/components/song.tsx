1eb497134f40259995d59278abc1864d72e0d73c
ibadus <ibadus@icloud.com>
2023-01-16 22:26:37 +0100

    fixed lint issues

--- src/components/song.tsx
+++ src/components/song.tsx
@@ -1,6 +1,7 @@
 import {SiSpotify} from 'react-icons/si';
 import {useLanyard} from 'use-lanyard';
 
+// Trunk-ignore(gitleaks/discord-client-id)
 export const DISCORD_ID = '229160979823460353';
 
 export function Song() {
@@ -9,7 +10,7 @@ export function Song() {
 	const c =
 		'text-right select-none sm:select-text w-full inline-flex text-lg rounded-sm focus:outline-none focus:opacity-100 focus:ring items-center space-x-2 no-underline opacity-50 hover:opacity-100 h-12 mt-0.5';
 
-	if (!user || !user.spotify) {
+	if (!user || user?.spotify === null) {
 		return (
 			<p className={c}>
 				<span>Not playing anything</span>
@@ -25,7 +26,7 @@ export function Song() {
 			target="_blank"
 			rel="noreferrer"
 			className={c}
-			href={`https://open.spotify.com/track/${user.spotify.track_id}`}
+			href={`https://open.spotify.com/track/${user.spotify.track_id ?? 'error'}`}
 		>
 			<span className="truncate">
 				<span className="text-gray-500">Listening to</span> {user.spotify.song} <span className="text-gray-500">by</span> {user.spotify.artist}
